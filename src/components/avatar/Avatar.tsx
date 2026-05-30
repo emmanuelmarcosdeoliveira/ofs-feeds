ceed3505e348e2f7b31c4e7115339b0e55ab5b40
Emmanuel Marcos de Oliveira <oliveirafullstack@gmail.com>
2026-05-30 12:30:06 -0300

    feat: refactor Avatar component to use ImgHTMLAttributes and improve props handling

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,11 +1,17 @@
-import styles from "./Avatar.module.css";
-export function Avatar({ hasBorder = true, src }) {
+import type { ImgHTMLAttributes } from 'react';
+import styles from './Avatar.module.css';
+
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
+  hasBorder?: boolean;
+}
+
+export function Avatar({ hasBorder = true, alt = '', ...props }: AvatarProps) {
   return (
     <div className={styles.avatar}>
       <img
         className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-        src={src}
-        alt="Imagem de perfil"
+        alt={alt}
+        {...props}
       />
     </div>
   );
